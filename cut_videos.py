ab386d1a856f60872e6a84aeb009525e813f304a
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-20 09:50:08 +0200

    Load an index file to extract many segments.

--- cut_videos.py
+++ cut_videos.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+import numpy as np
+from collections import defaultdict
 
 
 def get_extension(filepath):
@@ -8,6 +10,14 @@ def get_extension(filepath):
 
 
 def cut_one(filepath, number, start_time, duration):
+    # Extract a segment from a single video file, using the given start time
+    # and duration. The number is to create a unique output filename.
+
+    if not os.path.isfile(filepath):
+        print('File {} does not exist, returning now instead of causing '
+              'trouble.'.format(filepath))
+        return
+
     extension = get_extension(filepath)
 
     filepath_no_ext = str.replace(filepath, extension, '')
@@ -29,20 +39,21 @@ def cut_one(filepath, number, start_time, duration):
     return
 
 
-def cut():
+def cut(index_file):
 
-    # TODO: load a csv file.
+    data = np.genfromtxt(index_file, dtype=None, comments='#',
+                         names=('filepath', 'start_time', 'duration'))
 
-    filepath = 'videos/CK03-P1.mov'
-    number = 1
-    start_time = '00:02:40.0'
-    duration = 12
+    # Keeps track of how many times each file was cut.
+    file_counts = defaultdict(int)
 
-    print('Cutting')
-    cut_one(filepath, number, start_time, duration)
+    for (filepath, start_time, duration) in data:
+        number = file_counts[filepath]
+        file_counts[filepath] += 1
 
+        cut_one(filepath, number, start_time, duration)
     return
 
 
 if __name__ == '__main__':
-    cut()
+    cut('files_to_cut.txt')
